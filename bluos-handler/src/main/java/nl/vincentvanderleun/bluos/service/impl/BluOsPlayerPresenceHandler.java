05f0b8538aadd00eac7e6f6d512fbff13acd1381
Vincent van der Leun <22765050+vvdleun@users.noreply.github.com>
2024-08-29 15:54:56 +0200

    Added readme

--- bluos-handler/src/main/java/nl/vincentvanderleun/bluos/service/impl/BluOsPlayerPresenceHandler.java
+++
@@ -1,9 +0,0 @@
-package nl.vincentvanderleun.bluos.service.impl;
-
-import nl.vincentvanderleun.bluos.model.lsdp.LsdpAnnounceMessage;
-
-public class BluOsPlayerPresenceHandler {
-    public void handleAnnouncementMessage(LsdpAnnounceMessage message) {
-
-    }
-}
